0e7225fa8dceaf7dc93cc1a605b2767b38dce6ee
Brian Del Vecchio <bdv@hybernaut.com>
2015-09-11 20:37:56 -0400

    standardized docker setup

---
+++ home/docker-setup.sh
@@ -0,0 +1,10 @@
+# helpful shortcuts for docker, docker-compose, docker-machine
+
+eval "$(docker-machine env dev)"
+
+# docker / vagrant
+alias v="vagrant"
+alias d="docker"
+alias dc='docker-compose'
+alias dm='docker-machine'
+alias vgs="vagrant global-status"
