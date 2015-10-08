1c0a7f45d769464bd16e2a51e430b46692614eb2
Brian Del Vecchio <bdv@hybernaut.com>
2015-10-08 10:35:08 -0400

    tidying

--- home/docker-setup.sh
+++ home/docker-setup.sh
@@ -2,13 +2,16 @@
 
 eval "$(docker-machine env dev)"
 
-# docker / vagrant
-alias v="vagrant"
+# docker
 alias d="docker"
 alias dc='docker-compose'
 alias dm='docker-machine'
+
+# vagrant
+alias v="vagrant"
 alias vgs="vagrant global-status"
 
+# AWS ECS
 ECS_CLUSTER=default
 ecs() {
   aws ecs $* --cluster $ECS_CLUSTER
